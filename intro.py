f421a9785d115da4d72d528ff291b90fa0d5bb42
vebjørn <vebjorn.bh@gmail.com>
2026-01-15 11:14:35 +0100

    la til en liten intro

---
+++ intro.py
@@ -0,0 +1,41 @@
+# Variabler i Python
+tall = 5
+desimaltall = 3.14
+tekst = "Vebjørn"
+tekst2 = 'Hei på deg!'
+sant = True
+liste = [1, 2, 3, 4, 5]
+index = [0, 1, 2, 3, 4]
+
+
+# Dette er en kommentar
+
+""" 
+Dette kalles en docstring
+og kan brukes til å skrive lengre kommentarer
+over flere linjer.
+"""
+
+
+# print(tall)
+# print(desimaltall)
+# print(tekst)
+# print(sant)
+# print(liste)
+
+
+# Morsomme ting man kan gjøre med variabler
+
+#print(tall + 10)           # Legger til 10 til tall
+#print(tall + desimaltall)    # Legger sammen tall og desimaltall
+#print(tall*tall)        # Ganger tall med seg selv
+#print(tall**3)        # Opphøyer tall i tredje potens
+#print(tekst + " Blom-Hagen")    # Legger til " Blom-Hagen" til teksten
+#print("Hei, " + tekst + "!")  # Kombinerer tekst med andre strenger
+#print(tekst * 3)        # Gjentar teksten 3 ganger
+#print(liste + [6, 7, 8])   # Legger til flere elementer i listen
+#print(liste * 2)        # Gjentar listen 2 ganger
+#print(liste[0])        # Henter ut det første elementet i listen
+#print(liste[1:4])      # Henter ut elementene med index 1 til 3 (4 er ikke med)
+#print(len(liste))      # Finner lengden på listen
+
---
+++ oppgave_1.py
@@ -0,0 +1,17 @@
+#TODO definer en variabel
+tall =
+
+#TODO print variabelen
+
+# TODO endre variabelen og print den på nytt
+
+
+# a)
+#TODO print tallene 1 til 10
+
+# b)
+for n in range(10, 0, -1):
+    print(n)
+
+# c)
+#TODO definer en variabel med navnet ditt
